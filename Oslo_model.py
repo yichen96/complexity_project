4a259d93d721ba6aef46305bf1d7983e5fe38b2c
Yichen Liu <liuyichenpanda@gmail.com>
2017-02-07 18:16:18 +0000

    complete task up to 2b. but not cleaned up version

--- Oslo_model.py
+++ Oslo_model.py
@@ -45,6 +45,8 @@ class Oslo:
         self.threshold = np.ones(self.slopes.size) * random_generator(self.p)
         self.height_1 = 0  # height at first site i = 1 we only keep track the change in h
         self.avalanches = 0
+        self.t_c = 0
+        self.grain = 0
 
     def get_height(self):
         return np.sum(self.slopes)
@@ -54,6 +56,7 @@ class Oslo:
         self.threshold = np.ones(self.slopes.size) * random_generator(self.p)
         self.height_1 = 0
         self.avalanches = 0
+        self.grain = 0
 
     def add_grain(self, num_grains):
         avalanche = np.zeros(num_grains)
@@ -62,22 +65,40 @@ class Oslo:
             # drive
             self.slopes[0] += 1
             self.height_1 += 1
-            self.slopes, self.threshold, self.height_1, s = _relax(self.slopes, self.threshold, self.p, self.height_1)
-            avalanche[n_iter] = s
+            # self.slopes, self.threshold, self.height_1, s = _relax(self.slopes, self.threshold, self.p, self.height_1)
+            avalanche_counter = 0
+            while np.any(np.greater(self.slopes, self.threshold)):
+                for i in range(len(self.slopes)):
+                    if self.slopes[i] > self.threshold[i]:
+                        if i == 0:
+                            self.slopes[i] -= 2
+                            self.height_1 -= 1
+                            self.slopes[i + 1] += 1
+                            avalanche_counter += 1
+                        elif i == len(self.slopes) - 1:
+                            self.slopes[i] -= 1
+                            self.slopes[i - 1] += 1
+                            avalanche_counter += 1
+                            if self.t_c is 0:
+                                self.t_c = self.grain + n_iter
+                        else:
+                            self.slopes[i] -= 2
+                            self.slopes[i + 1] += 1
+                            self.slopes[i - 1] += 1
+                            avalanche_counter += 1
+                        self.threshold[i] = random_generator(self.p)
+            avalanche[n_iter] = avalanche_counter
             n_iter += 1
         if self.avalanches is 0:
             self.avalanches = avalanche
         else:
             self.avalanches = np.concatenate((self.avalanches, avalanche))
+        self.grain += num_grains
 
 
-def average_height(obj, num_grains_stdy_state=30000, sample_rate=100):
+def average_height(obj, num_grains_stdy_state=30000, sample_rate=1):
     obj.empty_model()
     height_list = []
-    # max_grains = num_grains_stdy_state + 100 * sample_rate
-    # for num_grains in np.arange(num_grains_stdy_state, max_grains + 1, sample_rate):
-    #     obj.add_grain(num_grains)
-    #     height_list.append(obj.get_height())
     obj.add_grain(num_grains_stdy_state)
     height_list.append(obj.get_height())
     for i in range(10):
@@ -87,18 +108,22 @@ def average_height(obj, num_grains_stdy_state=30000, sample_rate=100):
     obj.empty_model()
     return av_height
 
-
-a = Oslo(16)
-print a.get_height()
-a.add_grain(4)
-print a.slopes
-print a.avalanches
-a.add_grain(6)
-print a.slopes
-print a.avalanches
-a.empty_model()
-a.add_grain(2)
-print a.avalanches
+if __name__ == "__main__":
+    a = Oslo(16)
+    print a.get_height()
+    a.add_grain(0)
+    print a.get_height()
+    a.add_grain(4)
+    print a.slopes
+    print a.avalanches
+    a.add_grain(6)
+    print a.slopes
+    print a.avalanches
+    #a.empty_model()
+    a.add_grain(2500)
+    print a.avalanches
+    print a.t_c
+    print a.grain
 # a.empty_model()
 # a.add_grain(100)
 # print a.get_height()
